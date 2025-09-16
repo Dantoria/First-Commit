def5d124eefc5127581ce2ff45316ce1f8f227aa
Dantoria <zaidan.meiluano@gmail.com>
2025-09-16 14:25:57 +0700

    add dart1_comment

---
+++ 1_ Comment.dart
@@ -0,0 +1,19 @@
+// 1. Single line comment menggunakan //
+// 2. multi line comment menggunakan /* ... */
+// 3. Documentation comment menggunakan /// atau /** ...
+// */
+/**
+ * ini adalah multi line Comment
+ * yang ini lebih dari 1 baris
+ * program yang pertama adalah [main]
+ */
+
+void main() {
+  print ("dart is fun");
+
+  Print("5 * 5 = ${multiply()}"); 
+}
+
+int multiply(){
+
+}
