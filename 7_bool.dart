753fa8e9f5de305bbdb2f937460afcc85e63a7a2
Dantoria <zaidan.meiluano@gmail.com>
2025-09-19 10:30:58 +0700

    belajar boolean

---
+++ 7_bool.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  /**
+   * boolhanya ada 2 nilai-- true or false
+   */
+
+  
+
+  var istrue = true;
+
+  print(istrue);
+  print(!istrue);
+  print(true != false);
+}
