8046f543c63f1846ecb908645b027ca8d9f6b184
Dantoria <zaidan.meiluano@gmail.com>
2025-09-19 09:52:42 +0700

    belajar num and string

---
+++ 5_num.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  
+  num a = 10;
+  num b = 10.5;
+  num hexa = 0xDEADBEEF;
+
+  print(a);
+  print(b);
+  print(hexa);
+}
---
+++ 6_string.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  // ada single quote dan double
+  String singlequote = 'apa kabar kamu??';
+  String doublequote = "Baik";
+  String halo_baik = "halo, aku baik";
+
+  print('"apa kabar kamu?", tanya umar');
+
+  print(' $singlequote dan $doublequote');
+  
+  var kalkulasi = 'angka';
+  print('$kalkulasi = ${1 + 1}');
+
+  print('\u{1f600}');
+}
